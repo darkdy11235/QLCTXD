55382107ba8e7c470f73985f20e67e9e9039178d
darkdy11235 <darkdy11235@gmail.com>
2023-05-17 02:29:26 +0700

    update xe

--- src/db/ctxd.sql
+++ src/db/ctxd.sql
@@ -1,5 +1,5 @@
 drop database if exists CTXD;
-create database if not exists CTXD;
+create database if not exists CTXD utf8mb4 collate utf8mb4_unicode_ci;
 
 use CTXD;
 
@@ -58,11 +58,10 @@ create table if not exists xe (
     mo_ta nvarchar(255)  ,
     anh varchar(255)  ,
     gia_cho_thue_moi_gio int  ,
-    trang_thai nvarchar(50)   
+    trang_thai int   
 );
 
 -- alter trang_thai column in table xe = trang_thai enum('Chờ duyệt', 'Đã duyệt', 'Đã hủy') default 'Chờ duyệt' --
-alter table xe modify trang_thai enum('Chờ duyệt', 'Đã duyệt', 'Đã hủy') default 'Chờ duyệt';
 
 -- add foreign key constraint to table xe --
 alter table xe add constraint fk_xe_loai_xe foreign key (ma_loai_xe) references loai_xe(ma_loai_xe);
@@ -92,7 +91,7 @@ create table if not exists phieu_thue_xe (
     gio_thue int  ,
     gio_tra int  ,
     thoi_gian_tra_thuc_te datetime,
-    trang_thai nvarchar(50)   
+    trang_thai nvarchar(50)
 );
 
 -- alter trang_thai column in table enum('Đã đặt lịch', 'Đã hủy', 'Đang thuê', 'Đã trả') default 'Đã đặt lịch' --
@@ -129,16 +128,29 @@ alter table hoa_don modify trang_thai enum('Đã thanh toán', 'Chưa thanh toá
 -- add foreign key constraint to table hoa_don --
 alter table hoa_don add constraint fk_hoa_don_phieu_thue_xe foreign key (ma_phieu_thue_xe) references phieu_thue_xe(ma_phieu_thue_xe);
 
+insert into loai_xe (ten_loai_xe) values ('Xe địa hình'),
+                                    ('Xe đạp đua'),
+                                    ('Xe đạp thể thao'),
+                                    ('Xe đạp trợ lực điện'),
+                                    ('Xe đạp trẻ em'),
+                                    ('Xe đạp gấp'),
+                                    ('Xe đạp du lịch'),
+                                    ('Xe đạp leo núi'),
+                                    ('Xe đạp đường phố');
 
--- insert data into nguoi_dung table --
--- insert into nguoi_dung (ten_dang_nhap, mat_khau, ho_va_ten, cccd, sdt, email, dia_chi, trang_thai, quyen) values ('admin', 'admin', 'admin', '1234567890', '0999999999', 'admin@gmail.com', 'admin', 1, 'QTV');
+insert into nguoi_dung (ten_dang_nhap, mat_khau, ho_va_ten, cccd, sdt, email, dia_chi, trang_thai, quyen) values ('admin', '$2a$08$/62J/aKYb0hJSLsWX64QNuExUdMvw0hqoIvoiXjOzLL3akbTOQJFe', 'admin', '1234567890', '0999999999', 'admin@gmail.com', 'admin', 1, 'QTV'),
+                                                                                                                    ('user', '$2a$08$VfKxHZgyssybzNdn/Cgf2eVs8L4zNV4sME.50p6f5Paq8jFlqBRjW', 'user', '1234567891', '0999999998', 'user@gmail.com', 'user', 1, 'NT'),
+                                                                                                                    ('suser', '$2a$08$PsOfo.4VFJ3HIES2HrXRoe79u3m8iXZg5j8tHkZtqC6yUpKihI2zG', 'suser', '1234567892', '0999999997', 'suser@gmail.com', 'suser', 1, 'NCT');
 
--- alter tong_tien column in table hoa_don = thoi_gian_thue * gia_cho_thue_moi_gio + thoi_gian_thue_qua_han * gia_cho_thue_moi_gio * 1.5 --
--- alter table hoa_don add column thoi_gian_thue int  ;
--- alter table hoa_don add column thoi_gian_thue_qua_han int  ;
--- alter table hoa_don add column gia_cho_thue_moi_gio int  ;
--- alter table hoa_don add column gia_cho_thue_moi_gio_qua_han int  ;
--- update hoa_don set thoi_gian_thue = (select (ngay_tra - ngay_thue)*24 + gio_tra - gio_thue from phieu_thue_xe where phieu_thue_xe.ma = hoa_don.ma_phieu_thue_xe);
--- update hoa_don set thoi_gian_thue_qua_han = (select (day(thoi_gian_tra_thuc_te) - day(ngay_tra))*24 + hour(thoi_gian_tra_thuc_te) - gio_tra from phieu_thue_xe where phieu_thue_xe.ma = hoa_don.ma_phieu_thue_xe);
--- update hoa_don set gia_cho_thue_moi_gio = (select gia_cho_thue_moi_gio from xe where xe.ma = (select ma_xe from chi_tiet_phieu_thue_xe where chi_tiet_phieu_thue_xe.ma_phieu_thue_xe = hoa_don.ma_phieu_thue_xe));
--- update hoa_don set gia_cho_thue_moi_gio_qua_han = (select gia_cho_thue_moi_gio*1.5 from xe where xe.ma = (select ma_xe from chi_tiet_phieu_thue_xe where chi_tiet_phieu_thue_xe.ma_phieu_thue_xe = hoa_don.ma_phieu_thue_xe));
+insert into xe (ma_loai_xe, ma_nguoi_cho_thue, tieu_de, mo_ta, anh, gia_cho_thue_moi_gio, trang_thai) values (1, 3, 'Xe địa hình 1', 'Xe địa hình 1', 'https://cdn.tgdd.vn/Files/2021/07/20/1369474/phan-loai-cac-dong-xe-dap-pho-bien-tren-thi-5.png', 50000, 1),
+                                                                                                        (2, 3, 'Xe đạp đua 1', 'Xe đạp đua 1', 'https://cdn.tgdd.vn/Files/2021/07/20/1369474/phan-loai-cac-dong-xe-dap-pho-bien-tren-thi-8.jpg', 100000,1);
+
+insert into phieu_thue_xe(ma_nguoi_thue,ngay_thue,ngay_tra,gio_thue,gio_tra,trang_thai) values (2, '20230814', '20230814', 8,9,'Đã đặt lịch');
+
+insert into chi_tiet_phieu_thue_Xe(ma_phieu_thue_xe,ma_xe,ngay_thue,ngay_tra,gio_thue,gio_tra) values (1,1,'20230814','20230814',8,9),
+                                                                                                        (1,2,'20230814','20230814',8,9);
+
+-- trigger for hoa-don
+
+
+
--- src/models/xe.model.js
+++ src/models/xe.model.js
@@ -29,9 +29,10 @@ class XeModel{
         // xe(ma_xe,ma_loai_xe,ma_nguoi_cho_thue,tieu_de,mo_ta,anh,gia_cho_thue_moi_gio,trang_thai)
         // chi_tiet_phieu_thue_xe(ma_phieu_thue_xe,ma_xe,ngay_thue,ngay_tra,gio_thue,gio_tra)
         let sql = `SELECT * FROM ${this.tableName}
-        WHERE ma_xe NOT IN (SELECT ma_xe 
-                            FROM (Select * from chi_tiet_phieu_thue_Xe ctptx where ctptx.ngay_thue = ? and ctptx.ngay_tra = ?) 
-                            WHERE (gio_thue BETWEEN ? AND ?) or (gio_tra BETWEEN ? AND ?)`;
+        WHERE ma_xe NOT IN (SELECT ctptx_day.ma_xe 
+                            FROM (Select * from chi_tiet_phieu_thue_Xe ctptx where ctptx.ngay_thue = ? and ctptx.ngay_tra = ?) ctptx_day
+                            WHERE (ctptx_day.gio_thue > ? or ctptx_day.gio_thue < ?) or (ctptx_day.gio_tra > ? or ctptx_day.gio_tra < ?)) AND trang_thai = 1`;
+        console.log(ngay_thue, ngay_tra, gio_thue, gio_tra);
         return await query(sql, [ngay_thue, ngay_tra, gio_thue, gio_tra, gio_thue, gio_tra]);
     }
 
